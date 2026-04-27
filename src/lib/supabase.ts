35c6a70f2aa49256daa9e35a2d6cf7eb96e5a8a0
historias26 <historiasbiblicashoje25@gmail.com>
2026-04-27 10:16:15 -0300

    Corrigir chave anon

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://dpqpfpsgrwsjvpwdzkbc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwcXBmCHNyZ3NjanB3ZHprYmMiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4MDQxMTcwMCwiZXhwIjoyMDU5OTcxNzAwfQ.aa0c536e-377b-40c9-8776-412d41344ea7';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwcXBmcHNncndzanZwd2R6a2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyOTM1NTksImV4cCI6MjA5Mjg2OTU1OX0.3gIdm-vurSd3l5EtXkzkcPKarZnEsDo7L7iiwIdJuRM';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
