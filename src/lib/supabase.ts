aecb61bf093b982e6efcb6eecf030cbbde60f5ad
historias26 <historiasbiblicashoje25@gmail.com>
2026-04-27 10:19:15 -0300

    Usar service_role key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://dpqpfpsgrwsjvpwdzkbc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwcXBmcHNncndzanZwd2R6a2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyOTM1NTksImV4cCI6MjA5Mjg2OTU1OX0.3gIdm-vurSd3l5EtXkzkcPKarZnEsDo7L7iiwIdJuRM';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwcXBmcHNncndzanZwd2R6a2JjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzI5MzU1OSwiZXhwIjoyMDkyODY5NTU5fQ.4d1OR_LmKaZ1nFXi9FSntoW8JYAzgK_ZpNm334ht6xA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
